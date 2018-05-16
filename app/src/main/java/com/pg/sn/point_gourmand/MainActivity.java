9cc63e0122e4072d9ba6de7f66939e8a2251dd5c
mouhamed <mdwade37@gmail.com>
2018-05-16 23:17:04 +0000

    Initial commit

---
+++ app/src/main/java/com/pg/sn/point_gourmand/MainActivity.java
@@ -0,0 +1,42 @@
+package com.pg.sn.point_gourmand;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn, btnSignUp;
+    TextView slogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button) findViewById(R.id.btnSignIn);
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+
+        slogan = (TextView) findViewById(R.id.slogan);
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Nabila.ttf");
+        slogan.setTypeface(font);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
